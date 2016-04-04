f0a78d0442c96db469a8ada075dfc6c3f9457577
Amr Draz <amr.m.draz@gmail.com>
2016-04-04 12:59:20 +0200

    ok should be done now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.NODEJS_PORT || 80;
 const IP = process.env.NODEJS_IP || '0.0.0.0';
 
 function handleRequest(request, response) {
-	response.end(`Web server running on Node.js v${process.versions.node} deployed with wercker`);
+	response.end(`This is done autimatically`);
 }
 
 var server = http.createServer(handleRequest);
