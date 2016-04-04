18b28c34c5b3a5a9204340f3b003d18199c626df
Amr Draz <amr.m.draz@gmail.com>
2016-04-04 11:54:00 +0200

    modifed ip and port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const PORT = process.env.NODEJS_PORT || 80;
+const IP = process.env.NODEJS_IP || '0.0.0.0';
 
 function handleRequest(request, response) {
 	response.end(`Web server running on Node.js v${process.versions.node} deployed with wercker.`);
