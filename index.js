de2a4aea9beb0e159e07efb7a7f9a6729d36a4c2
Amr Draz <amr.m.draz@gmail.com>
2016-04-05 15:25:14 +0200

    removed space

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var http = require('http');
 const PORT = process.env.NODEJS_PORT || 80;
 const IP = process.env.NODEJS_IP || '0.0.0.0';
 
-
 function handleRequest(request, response) {
 	response.end(`This deployment done autimatically`);
 }
