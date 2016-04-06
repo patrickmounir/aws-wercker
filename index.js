98c701384fd7f356f049115d250c1d18392b2c08
Amr Draz <amr.m.draz@gmail.com>
2016-04-06 09:09:21 +0200

    changed index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var PORT = process.env.NODEJS_PORT || 80;
 var IP = process.env.NODEJS_IP || '0.0.0.0';
 
 function handleRequest(request, response) {
-	response.end(`This deployment was done autimatically`);
+	response.end(`This deployment was deployed by draz`);
 }
 
 var server = http.createServer(handleRequest);
