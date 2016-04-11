6074f5b13648f0c8258dde5e4fc39e89654f4046
Amr Draz <amr.m.draz@gmail.com>
2016-04-11 12:44:46 +0000

    minor change

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ var PORT = process.env.PORT || 80;
 var IP = process.env.IP || '0.0.0.0';
 
 function handleRequest(request, response) {
+	response.end(`This deployment is deployed by draz`);
+}
+
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, IP, function() {
+	console.log(`Server listening on http://${IP}:${PORT}`);
+});var http = require('http');
+
+var PORT = process.env.PORT || 80;
+var IP = process.env.IP || '0.0.0.0';
+
+function handleRequest(request, response) {
 	response.end(`This deployment is deployed by draz auto`);
 }
 
