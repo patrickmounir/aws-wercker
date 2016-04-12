c93a9a4a17299d58fa590d537e8a20788dd52cc8
Amr Draz <amr.m.draz@gmail.com>
2016-04-12 11:37:29 +0000

    failing test

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require("assert");
 describe('Array', function() {
 	describe('#indexOf()', function() {
 		it('should return -1 when the value is not present', function() {
-			assert.equal(true, true);
+			assert.equal(true, false);
 			assert.equal(false, false);
 		});
 	});
