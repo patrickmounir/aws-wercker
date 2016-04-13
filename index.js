6623b4a62e1c56ae1c6b1335437b65a2d5426f4a
Amr Draz <amr.m.draz@gmail.com>
2016-04-13 15:14:31 +0000

    made change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 80;
 var IP = process.env.IP || '0.0.0.0';
 
 function handleRequest(request, response) {
-	response.end(`This app is deployed by Draz via wercker`);
+	response.end(`This app is deployed by Draz via wercker autimatical`);
 }
 
 
